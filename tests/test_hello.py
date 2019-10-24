5187ca39b26dead483cc53a9d3f255d7529e15fe
Stephen Richer <sr467@bath.ac.uk>
2019-10-24 17:07:03 +0100

    Added new tests for stdout and comparing expected data.

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,9 +1,21 @@
 import pyTemplateBath.hello
 import pytest
 import pyCommonTools.logging
+from pyCommonTools.testing import datadir
 
-def test_run():
-    assert pyTemplateBath.hello.hello_world()
+def test_hello_world(capsys):
+    
+    """ Compare stdout with expected value. """
+    
+    pyTemplateBath.hello.hello_world()
+    captured = capsys.readouterr()
+    assert captured.out == "Hello World.\n"
 
-def test_install_requires():
-    assert hasattr(pyCommonTools.logging.create_logger, '__call__')
+def test_data(datadir):
+    
+    """ Compare line by line, with expected data file. """
+    
+    expected_data = datadir.join('test.dat')
+    with open(expected_data) as f:
+        for line in ['line1', 'line2']:
+            assert f.readline().strip('\n') == line

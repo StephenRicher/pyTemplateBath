4fbd46d8b197f32ac1aa68df1d58a64e8f482f45
Stephen Richer <sr467@bath.ac.uk>
2019-10-24 15:07:18 +0100

    Add test for install requires in setup.py.

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,9 @@
 import pyTemplateBath.hello
 import pytest
+import pyCommonTools.logging
 
 def test_run():
     assert pyTemplateBath.hello.hello_world()
+
+def test_install_requires():
+	assert hasattr(pyCommonTools.logging.create_logger, '__call__')

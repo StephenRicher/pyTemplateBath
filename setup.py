775f3f8bbecf6203e14162ca06909662cc2640c7
Stephen Richer <sr467@bath.ac.uk>
2019-10-24 15:25:14 +0100

    Added back in install requires.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url = 'https://github.com/StephenRicher/pyTemplate',
     scripts = ['bin/pyTemplateBath'],
     python_requires = '>=3.3.0',
-    install_requires = [],
+    install_requires = ['pyCommonTools'],
     setup_requires = ['pytest-runner'],
     tests_require = ['pytest'],
     license = 'MIT',

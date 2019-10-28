d740f2875cf684c5e006ed75e0858edc57b7315c
Stephen Richer <sr467@bath.ac.uk>
2019-10-28 12:37:44 +0000

    Attempting to fix travis.

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,21 +1,20 @@
 import pytest
 import pyTemplateBath as tb
-import pyCommonTools as pct
-
+from pyCommonTools import datadir
 
 def test_hello_world(capsys):
-    
+
     """ Compare stdout with expected value. """
-    
+
     tb.hello.hello_world()
     captured = capsys.readouterr()
     assert captured.out == "Hello World.\n"
 
-def test_data(pct.datadir):
-    
+def test_data(datadir):
+
     """ Compare line by line, with expected data file. """
-    
-    expected_data = pct.datadir.join('test.dat')
+
+    expected_data = datadir.join('test.dat')
     with open(expected_data) as f:
         for line in ['line1', 'line2']:
             assert f.readline().strip('\n') == line
